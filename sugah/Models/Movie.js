bd78475548136b3fe82c73b454692893f2d0d64c
IuliaV <iuliamvoicu@yahoo.com>
2017-10-12 16:19:06 +0000

    adding search module

---
+++ sugah/Models/Movie.js
@@ -0,0 +1,21 @@
+function Movie(defaultData) {
+    defaultData = defaultData || {};
+    this.Title = defaultData.Title;
+    this.Genre = defaultData.Genre;
+    this.Language = defaultData.Language;
+    this.Country = defaultData.Country;
+    this.Year = defaultData.Year;
+    this.Runtime = defaultData.Runtime;
+    this.Type = defaultData.Type;
+    this.Poster = defaultData.Poster;
+    this.imdbID = defaultData.imdbID;
+    this.imdbRating - defaultData.imdbRating;
+    this.imdbVotes = defaultData.imdbVotes;
+    this._id = defaultData._id;
+}
+
+
+
+
+
+
