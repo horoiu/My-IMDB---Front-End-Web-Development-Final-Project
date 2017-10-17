864c455150912306ef99d6a2f891bca5e4e1003b
IuliaV <iuliamvoicu@yahoo.com>
2017-10-17 20:24:33 +0000

    cleanup, updated Movie model

--- Group 2 My IMDB/Models/Movie.js
+++ Group 2 My IMDB/Models/Movie.js
@@ -1,20 +1,32 @@
-function Movie(defaultData) {
-    defaultData = defaultData || {};
+function Movie(defaultData){
+    defaultData = defaultData||{};
+    this._id = defaultData._id;
     this.Title = defaultData.Title;
+    this.Year = defaultData.Year;
+    this.Rated = defaultData.Rated;
+    this.Released = defaultData.Released;
+    this.Runtime = defaultData.Runtime;
     this.Genre = defaultData.Genre;
+    this.Director = defaultData.Director;
+    this.Writer = defaultData.Writer;
+    this.Actors = defaultData.Actors;
+    this.Plot = defaultData.Plot;
     this.Language = defaultData.Language;
     this.Country = defaultData.Country;
-    this.Year = defaultData.Year;
-    this.Runtime = defaultData.Runtime;
-    this.Type = defaultData.Type;
+    this.Awards = defaultData.Awards;
     this.Poster = defaultData.Poster;
-    this.imdbID = defaultData.imdbID;
-    this.imdbRating - defaultData.imdbRating;
+    this.Source = defaultData.Source;
+    this.Value = defaultData.Value;
+    this.Metascore = defaultData.Metascore;
+    this.imdbRating = defaultData.imdbRating;
     this.imdbVotes = defaultData.imdbVotes;
-    this._id = defaultData._id;
+    this.imdbID = defaultData.imdbID;
+    this.Type = defaultData.Type;
+    this.DVD = defaultData.DVD;
+    this.BoxOffice = defaultData.BoxOffice;
+    this.Production = defaultData.Production;
+    this.Webiste = defaultData.Website;
 }
-
-
 
 
 
--- Group 2 My IMDB/Views/newSearch.js
+++ Group 2 My IMDB/Views/newSearch.js
@@ -2,82 +2,15 @@
 window.addEventListener("load", function() {
     var searchBtn = document.getElementsByClassName("js_search_btn")[0];
     var searchInput = document.getElementsByClassName("js_search_input")[0];
-    var genreArr = ["Action", "Adventure", "Animation", "Biography", "Comedy", "Crime", "Documentary", "Drama", "Family", "Fantasy", "Film-Noir", "History", "Horror", "Music", "Musical", "Mystery", "Romance", "Sci-Fi", "Sport", "Thriller", "Western",];
-    
-    // /* display first */
-    // var allMovies = new DataBase();
-    // allMovies.getMovies("", "All").then(showResults).catch(function(error){console.log("Data base error: " + error)});
-    // function showResults(response) {
-    //         var parent = document.getElementById("search-results");
-            
-    //         /* initialise search results array */
-    //         var resultsArr = [];
-    //         for (var i=0; i<response.moviesList.length; i++) {
-    //             resultsArr.push(response.moviesList[i]);
-    //         }
-            
-    //         /* purge doubles from response */
-    //         for (var j=0; j<resultsArr.length; j++) {
-    //             for (var k=j+1; k<resultsArr.length; k++) {
-    //               if (resultsArr[j].Title === resultsArr[k].Title && resultsArr[j].Year === resultsArr[k].Year) {
-    //                   resultsArr.splice(k,1);
-    //                   k=j;
-    //                 } 
-    //             }
-                
-    //         }
-            
-    //         /* check whether we found or not movies according to search and render them */
-    //         if (resultsArr.length >0) {
-    //             console.log(resultsArr);
-    //             renderResults(resultsArr);
-    //         } else {
-    //             console.log("No movies match your search");
-    //           }
-
-           
-    //         /* render search results - clear previous search*/ 
-    //         function renderResults(arr) {
-    //             while (parent.firstChild) {
-    //                 parent.removeChild(parent.firstChild);
-    //             }
-    //             for (var i=0; i<arr.length;i++) {
-    //                 let renderMovie = document.createElement("p");
-    //                 renderMovie.innerHTML = arr[i].Title;
-    //                 renderMovie.setAttribute("id", arr[i]._id);
-    //                 renderMovie.addEventListener("click", goToMovieDetails);
-    //                 parent.appendChild(renderMovie);
-    //             }
-               
-    //         }
-            
-            
-    //         /* *************************************MOVIE DETAILS PART************************************************************************** */
-    //         function goToMovieDetails(e) {
-    //             var wantedMovieId = e.target.id;
-    //             // window.open("../movieDetails.html?id="+wantedMovieId, "_blank");
-               
-    //             window.open("./movieDetails.html", "_blank");
-    //             var movie = new MovieDetails;
-    //             movie.getDetails(wantedMovieId).then(renderMovie);
-    //             function renderMovie(response) {
-    //                 console.log(response.reqMovie);
-    //             }
-               
-    //         }
-            
-            
-            
-            
-    //     }  
-    
     searchBtn.addEventListener("click", startSearch);
     searchInput.addEventListener("keydown", inputSearch);
+    /* start search if user pushes Enter also */
     function inputSearch(event){
         if (event.key === "Enter") { 
             startSearch(event);
         }
     }
+    /* search function */
     function startSearch(e) {
         e.preventDefault();
         var searchMovie=searchInput.value;
@@ -88,7 +21,7 @@ window.addEventListener("load", function() {
         /* get the results according to search params : title and/or genre */
         allMovies.getMovies(searchMovie, selectedGenre).then(showResults).catch(function(error){console.log("Data base error: " + error)});
         
-        
+        /* results check and render function */
         function showResults(response) {
             var parent = document.getElementById("search-results");
             
@@ -116,7 +49,6 @@ window.addEventListener("load", function() {
             } else {
                 console.log("No movies match your search");
               }
-
            
             /* render search results - clear previous search*/ 
             function renderResults(arr) {
